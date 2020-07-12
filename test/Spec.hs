474f0afb4ca8c364713e18573a99b5cefb797bc3
Ignacio Slater Muñoz <roac@Ignacio-PC.localdomain>
2020-07-12 13:06:11 -0400

    Basic configs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+-- "Probamon" (c) by Ignacio Slater M.
+
+-- "Probamon" is licensed under a
+-- Creative Commons Attribution 4.0 International License.
+
+-- You should have received a copy of the license along with this
+-- work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+
+-- v1.0
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
